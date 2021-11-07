da514264f7cf0f933379fc2df87a314fbeeaec7b
d3fau1t <aciddust20@gmail.com>
2021-11-08 01:06:21 +0900

    Revert ":bug: If getting huge payload, get resize your image"
    This reverts commit 48d68c6c791723089b53c2b1178807a3d6dd49c7.

--- binjector/utils.py
+++ binjector/utils.py
@@ -1,6 +1,4 @@
 import json
-from math import ceil
-from typing import Tuple
 from hashlib import md5
 from datetime import datetime
 
@@ -35,24 +33,3 @@ def set_token():
     with open("./settings.json", "w") as setting_file:
         json.dump(settings, setting_file)
     print ("Token set to: " + settings['token'])
-
-def message_to_binary(message: str) -> str:
-        """
-        Convert your message to a binary things.
-        """
-        return ''.join(format(ord(char), '08b') for char in message)
-
-def get_resized_ratio(image_size: Tuple[int, int], message: str) -> Tuple[bool, Tuple[int, int]]:
-    '''
-    if need resize, return (True, new size)
-    if not need resize, return (False, None)
-    '''
-    colors = 3
-    width, height = image_size
-    message_length = len(message_to_binary(message))
-    ratio = 1.0
-    while ceil(width*ratio) * ceil(height*ratio) * colors < message_length:
-        ratio += 0.5
-    if ratio > 1.0:
-        return (True, (ceil(width*ratio), ceil(height*ratio)))
-    return (False, (width, height))
